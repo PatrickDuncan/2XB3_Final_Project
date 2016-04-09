45921af8d0d83798ce78c3426e91c3cbaa4baf12
Patrick Duncan <patrick.duncan@outlook.com>
2016-04-09 19:07:08 -0400

    Popup/modal window added and searching improved

--- WebContent/js/entries.js
+++ WebContent/js/entries.js
@@ -1,19 +1,31 @@
-var csvdata;
-var i;
+var csv;
 var row;
+
+/**
+ * The initial loading in other the data set.
+ */
 function initial() {
-	var csv = "assets/Consumer_Complaints.csv";
+	var initial = "assets/Consumer_Complaints0.csv";
 	row = 0;
-	$.get(csv, function(data) {
-		csvdata = Papa.parse(data);
+	$.get(initial, function(data) {
+		csv = Papa.parse(data);
 		display();
 	});
+	setTimeout(function wait() {
+		for (var j = 1; j < 6; j++) {
+			$.get("assets/Consumer_Complaints" + j + ".csv", function(data) {
+				var temp = Papa.parse(data);
+				for (var k = 0; k < temp.data.length; k++)
+					csv.data.push(temp.data[k]);
+			});
+		}
+	}, 2000);
+
 }
 /**
  * Display the next 500 entries from the row index in a table
  */
 function display() {
-	// console.log(csvdata.data);
 	var display = "";
 	display += "<thread>" + "<tr id='head'>" + "<th><span class='text'>"
 			+ "Date Received" + "</span></th>" + "<th><span class='text'>"
@@ -23,23 +35,46 @@ function display() {
 			+ "</span></th>" + "<th><span class='text'>" + "State"
 			+ "</span></th>" + "</tr>" + "</thread>";
 	display += "<tbody>";
-	if (row < csvdata.data.length - 500) {
+	if (row < csv.data.length - 500) {
 		for (var i = row; i < row + 501; i++) {
-			display += "<tr id='shift'>" + "<td>" + csvdata.data[i][0]
-					+ "</td>" + "<td>" + csvdata.data[i][1] + "</td>" + "<td>"
-					+ csvdata.data[i][3] + "</td>" + "<td>"
-					+ csvdata.data[i][4] + "</td>" + "<td>"
-					+ csvdata.data[i][7] + "</td>" + "<td>"
-					+ csvdata.data[i][8] + "</td>" + "</tr>";
+			display += "<tr id='entry'>" + "<td><a onclick='pop(" + i + ")'" +
+					" href='#'data-toggle='modal' data-target='.popup'>"
+					 + csv.data[i][0] + "</a></td>"
+					 
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][1] + "</a></td>"
+							
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][3] + "</a></td>" 
+							
+					+ "<td><a onclick='pop(" + i + ")' " + "href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][4] + "</a></td>"
+					
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][7] + "</a></td>" 
+					
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][8] + "</a></td>" + "</tr>";
 		}
 	} else {
-		for (var i = row; i < csvdata.data.length; i++) {
-			display += "<tr id='shift'>" + "<td>" + csvdata.data[i][0]
-					+ "</td>" + "<td>" + csvdata.data[i][1] + "</td>" + "<td>"
-					+ csvdata.data[i][3] + "</td>" + "<td>"
-					+ csvdata.data[i][4] + "</td>" + "<td>"
-					+ csvdata.data[i][7] + "</td>" + "<td>"
-					+ csvdata.data[i][8] + "</td>" + "</tr>";
+		for (var i = row; i < csv.data.length; i++) {
+			display += "<tr id='entry'><td><a onclick='pop(" + i + ")'" +
+					" href='#'data-toggle='modal' data-target='.popup'>" + csv.data[i][0] + "</a></td>"
+					
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][1] + "</a></td>"
+							
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][3] + "</a></td>" 
+					
+					+ "<td><a onclick='pop(" + i + ")' " + "href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][4] + "</a></td>"
+							
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][7] + "</a></td>"
+							
+					+ "<td><a onclick='pop(" + i + ")' href='#'data-toggle='modal'" +
+							" data-target='.popup'>" + csv.data[i][8] + "</a></td>" + "</tr>";
 		}
 	}
 	display += "</tbody>";
@@ -47,17 +82,36 @@ function display() {
 	document.getElementById("loading").style.display = "none";
 	document.getElementById("searching").style.display = "none";
 }
+
+/**
+ * Creates the content of the popup window when you press an entry
+ * 
+ * @param i the index of the entry
+ */
+function pop(i) {
+	var display="<h1><b>" + csv.data[i][7] + "</b><br>" + "</h1>";
+	display += "<h2>" + csv.data[i][0] + "</h2>";
+	display += "<h2><b>" + csv.data[i][1]  + "</b>" + "</h2>";
+	display += '<h3><b>Consumer Response: "' + csv.data[i][5] + '"</b></h3>';
+	display += '<h3><b>Company Response: "' + csv.data[i][6] + '"</b></h3><br>';
+	display += '<h4>State: ' + csv.data[i][8] + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" +
+			"Zip Code: " + csv.data[i][9] + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" +
+			"Timely Response: " + csv.data[i][15] + "</h4>";
+	document.getElementById("work").innerHTML = display;
+}
+
 /**
  * Sort the array by the x column
+ * 
  * @param x either 0,1,3,4,7,8 i.e. which column to sort by
  */
 function sort(x) {
 	i = x;
 	document.getElementById("loading").style.display = "block";
-	if (document.getElementById("zero").checked) {
+	if (document.getElementById("min").checked) {
 		if (i == 0) { // If sorting the date
 			setTimeout(function wait() {
-				csvdata.data.sort(function(a, b) {
+				csv.data.sort(function(a, b) {
 					var m = a[i].split("/");
 					var n = b[i].split("/");
 					// At 2 is the year in the date
@@ -67,16 +121,16 @@ function sort(x) {
 			}, 0);
 		} else {
 			setTimeout(function wait() {
-				csvdata.data.sort(function(a, b) {
+				csv.data.sort(function(a, b) {
 					return a[i].localeCompare(b[i]);
 				});
 				display();
 			}, 0);
 		}
-	} else if (document.getElementById("one").checked) {
+	} else if (document.getElementById("max").checked) {
 		if (i == 0) { // If sorting the date
 			setTimeout(function wait() {
-				csvdata.data.sort(function(a, b) {
+				csv.data.sort(function(a, b) {
 					var m = a[i].split("/");
 					var n = b[i].split("/");
 					// At 2 is the year in the date
@@ -86,7 +140,7 @@ function sort(x) {
 			}, 0);
 		} else {
 			setTimeout(function wait() {
-				csvdata.data.sort(function(a, b) {
+				csv.data.sort(function(a, b) {
 					return b[i].localeCompare(a[i]);
 				});
 				display();
@@ -97,11 +151,12 @@ function sort(x) {
 }
 /**
  * Set the row index to display the next page of entries
- * @param next
+ * 
+ * @param next if going to te previous or next page
  */
 function step(next) {
 	if (next) {
-		if (row < csvdata.data.length - 500) {
+		if (row < csv.data.length - 500) {
 			row = row + 500;
 			display();
 		}
@@ -112,15 +167,29 @@ function step(next) {
 		}
 	}
 }
-
-var keyword;
-function searchC() {
+/**
+ * Searches for the keyword in the search bar.
+ * It searches the current page for the keyword and if its not found
+ * it goes through the other pages until it hits the length of the
+ * csv object then it starts from the beginning up until the
+ * starting point. If nothing is found nothing happens.
+ */
+function search() {
 	document.getElementById("searching").style.display = "block";
-	keyword = document.getElementById("findC").value;
+	var keyword = document.getElementById("find").value;
+	var i=-1;
+	if (document.getElementById("zero").checked)
+		i = 1;
+	else if (document.getElementById("one").checked)
+		i = 3;
+	else if (document.getElementById("two").checked)
+		i = 7;
+	else if (document.getElementById("three").checked)
+		i = 8;
 	var found = false;
 	setTimeout(function wait() {
-		for (var j=row; j<csvdata.data.length; j++) {
-			var m = csvdata.data[j][7].toLowerCase();
+		for (var j = row; j < csv.data.length; j++) {
+			var m = csv.data[j][i].toLowerCase();
 			var n = keyword.toLowerCase();
 			if (m.indexOf(n) > -1) {
 				row = j;
@@ -129,36 +198,8 @@ function searchC() {
 			}
 		}
 		if (!found) {
-			for (var j=0; j<row; j++) {
-				var m = csvdata.data[j][7].toLowerCase();
-				var n = keyword.toLowerCase();
-				if (m.indexOf(n) > -1) {
-					row = j;
-					break;
-				}
-			}
-		}
-		display();
-	}, 0);
-}
-
-function searchP() {
-	document.getElementById("searching").style.display = "block";
-	keyword = document.getElementById("findP").value;
-	var found = false;
-	setTimeout(function wait() {
-		for (var j=row; j<csvdata.data.length; j++) {
-			var m = csvdata.data[j][1].toLowerCase();
-			var n = keyword.toLowerCase();
-			if (m.indexOf(n) > -1) {
-				row = j;
-				found = true;
-				break;
-			}
-		}
-		if (!found) {
-			for (var j=0; j<row; j++) {
-				var m = csvdata.data[j][1].toLowerCase();
+			for (var j = 0; j < row; j++) {
+				var m = csv.data[j][i].toLowerCase();
 				var n = keyword.toLowerCase();
 				if (m.indexOf(n) > -1) {
 					row = j;
--- WebContent/js/oneCheck.js
+++ WebContent/js/oneCheck.js
@@ -1,18 +1,34 @@
-//http://stackoverflow.com/questions/9709209/html-select-only-one-checkbox-in-a-group
+//Heavily modified from: http://stackoverflow.com/questions/9709209/html-select-only-one-checkbox-in-a-group
 $("input:checkbox").on('click', function() {
 	var $box = $(this);
 	if ($box.is(":checked")) {
 		var group = "input:checkbox[name='" + $box.attr("name") + "']";
 		$(group).prop("checked", false);
 		$box.prop("checked", true);
-		var label = document.getElementById("optionsLabel");
-		if (label.innerHTML == 'A - Z <span class="caret"></span>') {
-			label.innerHTML = 'Z - A <span class="caret"></span>';
-		} else {
-			label.innerHTML = 'A - Z <span class="caret"></span>';
+		if ($box.attr("name") == "minMax") {
+			var label = document.getElementById("optionsLabel");
+			if (label.innerHTML == 'A - Z <span class="caret"></span>')
+				label.innerHTML = 'Z - A <span class="caret"></span>';
+			else
+				label.innerHTML = 'A - Z <span class="caret"></span>';
+		} else if ($box.attr("name") == "sorting") {
+			var id = $box.attr("id");
+			var label = document.getElementById("optionsLabel2");
+			var searchBar = document.getElementById("find");
+			if (id == "zero") {
+				label.innerHTML = 'Company <span class="caret"></span>';
+				searchBar.placeholder = "Search for Company...";
+			} else if (id == "one") {
+				label.innerHTML = 'Issue <span class="caret"></span>';
+				searchBar.placeholder = "Search for Issue...";
+			} else if (id == "two") {
+				label.innerHTML = 'Product <span class="caret"></span>';
+				searchBar.placeholder = "Search for Product...";
+			} else if (id == "three") {
+				label.innerHTML = 'State <span class="caret"></span>';
+				searchBar.placeholder = "Search for State...";
+			}
 		}
-	}
-	else {
+	} else
 		$box.prop("checked", true);
-	}
 });
